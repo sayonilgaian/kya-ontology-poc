fb9f5dde88254177bc1ae1f75c2812d1b50017b5
sayonilgaian <sayonil.m@mobiusdtaas.ai>
2025-08-04 16:12:25 +0530

    initial rendering of graph using cytoscape v3

--- src/ThirdPartyServices/cytoscapeCanvas/cytoscapeDataUtils/defaultStyles.ts
+++ src/ThirdPartyServices/cytoscapeCanvas/cytoscapeDataUtils/defaultStyles.ts
@@ -43,4 +43,23 @@ const cytoscapeDefaultStyles = [
     }
 ]
 
-export default cytoscapeDefaultStyles
+const cytoscapeDefaultLayout = {
+    name: 'cose',
+    idealEdgeLength: 100,
+    nodeOverlap: 20,
+    refresh: 20,
+    fit: true,
+    padding: 30,
+    randomize: false,
+    componentSpacing: 40,
+    nodeRepulsion: 400000,
+    edgeElasticity: 100,
+    nestingFactor: 5,
+    gravity: 80,
+    numIter: 1000,
+    initialTemp: 200,
+    coolingFactor: 0.95,
+    minTemp: 1.0
+}
+
+export { cytoscapeDefaultStyles, cytoscapeDefaultLayout }
