46d41953065331b5dedfce043042b2cbdc33eca1
sayonilgaian <sayonil.m@mobiusdtaas.ai>
2025-08-04 13:06:02 +0530

    added cytoscape component draft v1

--- src/ThirdPartyServices/index.ts
+++ src/ThirdPartyServices/index.ts
@@ -1,9 +1,11 @@
 import { ForceGraphService } from "./3dForceGraph/3dForceGraph";
+import { CytoscapeService } from "./3dForceGraph/cytoscapeCanvas";
 import { InstanceManager } from "./InstanceManager";
 
 
 const instanceManger = new InstanceManager();
 
 instanceManger.registerDependency("3dForceGraph", ForceGraphService)
+instanceManger.registerDependency("cytoscapeCanvas", CytoscapeService)
 
 export { instanceManger };
